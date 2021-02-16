404a7c1904f8ec89ef6ac48576f9f7cb601c5ba5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-16 08:47:18 +0200

    no message

--- src/Blauhaus.SignalR.Abstractions/Server/Handlers/ISyncRequestHandler.cs
+++ src/Blauhaus.SignalR.Abstractions/Server/Handlers/ISyncRequestHandler.cs
@@ -3,7 +3,8 @@ using Blauhaus.SignalR.Abstractions.Sync;
 
 namespace Blauhaus.SignalR.Abstractions.Server.Handlers
 {
-    public interface ISyncRequestHandler<TDto> : IConnectedUserCommandHandler<SyncResponse<TDto>, SyncRequest> where TDto : ISyncClientEntity
+    public interface ISyncRequestHandler<TDto> : IConnectedUserCommandHandler<SyncResponse<TDto>, SyncRequest> 
+        where TDto : IClientEntity
     {
         
     }
