d259d8aef3d2bf842e4be1f08465d06cc2048c69
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-16 09:33:51 +0200

    no message

---
+++ src/Blauhaus.SignalR.TestHelpers/MockBuilders/HubContextProxyMockBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Blauhaus.Responses;
+using Blauhaus.SignalR.Abstractions.Server;
+using Blauhaus.TestHelpers.MockBuilders;
+using Moq;
+
+namespace Blauhaus.SignalR.TestHelpers.MockBuilders
+{
+    public class HubContextProxyMockBuilder : BaseMockBuilder<HubContextProxyMockBuilder, IHubContextProxy>
+    {
+        public HubContextProxyMockBuilder Where_SendAsync_returns<TDto>(Response response)
+        {
+            Mock.Setup(x => x.SendDtoAsync(It.IsAny<string>(), It.IsAny<TDto>()))
+                .ReturnsAsync(response);
+            return this;
+        }
+    }
+}
