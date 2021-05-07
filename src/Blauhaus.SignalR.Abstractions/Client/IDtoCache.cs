1423236d41c8d5974e79f45fcf7a588f8d93836f
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-07 08:34:24 +0200

    no message

--- src/Blauhaus.SignalR.Abstractions/Client/IDtoCache.cs
+++ src/Blauhaus.SignalR.Abstractions/Client/IDtoCache.cs
@@ -8,8 +8,9 @@ namespace Blauhaus.SignalR.Abstractions.Client
     public interface IDtoCache<TDto, in TId> : IAsyncPublisher<TDto>
         where TDto : class, IHasId<TId>
     {
-        Task<TDto?> GetOneAsync(TId id);
         Task SaveAsync(TDto dto);
+
+        Task<TDto?> GetOneAsync(TId id);
         Task<IReadOnlyList<TDto>> GetAllAsync();
         Task<IReadOnlyList<TDto>> GetWhereAsync(Func<TDto, bool> search);
     }
