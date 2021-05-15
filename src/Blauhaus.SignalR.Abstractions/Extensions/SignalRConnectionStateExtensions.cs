8e99f92ff4c6c24d7a4ac660ad65cd527dac2347
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-15 15:06:39 +0200

    no message

---
+++ src/Blauhaus.SignalR.Abstractions/Extensions/SignalRConnectionStateExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Blauhaus.SignalR.Abstractions.Client;
+
+namespace Blauhaus.SignalR.Abstractions.Extensions
+{
+    public static class SignalRConnectionStateExtensions
+    {
+        public static bool IsConnecting(this SignalRConnectionState state)
+        {
+            return state is SignalRConnectionState.Connecting or SignalRConnectionState.Reconnecting;
+        }
+
+        public static bool IsConnected(this SignalRConnectionState state)
+        {
+            return state is SignalRConnectionState.Connected or SignalRConnectionState.Reconnected;
+        }
+        
+        public static bool IsDisconnecting(this SignalRConnectionState state)
+        {
+            return state is SignalRConnectionState.Disconnecting;
+        }
+
+        public static bool IsDisconnected(this SignalRConnectionState state)
+        {
+            return state is SignalRConnectionState.Disconnected;
+        }
+    }
+}
