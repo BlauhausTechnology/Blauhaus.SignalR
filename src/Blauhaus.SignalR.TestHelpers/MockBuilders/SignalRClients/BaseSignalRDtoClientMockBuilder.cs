dc1ee2b44592c5ab5fd57a24ccdac678668f04eb
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-09-10 12:50:51 +0200

    no message

--- src/Blauhaus.SignalR.TestHelpers/MockBuilders/SignalRClients/BaseSignalRDtoClientMockBuilder.cs
+++ src/Blauhaus.SignalR.TestHelpers/MockBuilders/SignalRClients/BaseSignalRDtoClientMockBuilder.cs
@@ -6,6 +6,7 @@ using Blauhaus.Common.TestHelpers.MockBuilders;
 using Blauhaus.Errors;
 using Blauhaus.Responses;
 using Blauhaus.SignalR.Abstractions.Client;
+using Blauhaus.TestHelpers.Builders.Base;
 using Blauhaus.TestHelpers.MockBuilders;
 using Moq;
 
@@ -18,14 +19,26 @@ namespace Blauhaus.SignalR.TestHelpers.MockBuilders.SignalRClients
         where TMock : class, ISignalRDtoClient<TDto>
     {
         
-        public TBuilder Where_HandleCommandAsync_returns<TCommand>(TDto payload)
+        public TBuilder Where_HandleCommandAsync_succeeds<TCommand>(TDto payload) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
                 .ReturnsAsync(Response.Success(payload));
             return (TBuilder) this;
         }
-        
-        public TBuilder Where_HandleCommandAsync_returns<TCommand>(List<TDto> payloads)
+        public TBuilder Where_HandleCommandAsync_succeeds<TCommand>(Func<TDto> payload) where TCommand : notnull
+        {
+            Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
+                .ReturnsAsync(()=> Response.Success(payload.Invoke()));
+            return (TBuilder) this;
+        }
+        public TBuilder Where_HandleCommandAsync_succeeds<TCommand>(IBuilder<TDto> payload) where TCommand : notnull
+        {
+            Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
+                .ReturnsAsync(()=> Response.Success(payload.Object));
+            return (TBuilder) this;
+        }
+
+        public TBuilder Where_HandleCommandAsync_returns<TCommand>(List<TDto> payloads) where TCommand : notnull
         {
             var queue = new Queue<Response<TDto>>();
             foreach (var payload in payloads)
@@ -37,27 +50,34 @@ namespace Blauhaus.SignalR.TestHelpers.MockBuilders.SignalRClients
             return (TBuilder) this;
         }
         
-        public TBuilder Where_HandleCommandAsync_returns<TCommand>(Response<TDto> payload)
+        public TBuilder Where_HandleCommandAsync_returns<TCommand>(Response<TDto> payload) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
                 .ReturnsAsync(payload);
             return (TBuilder) this;
         }
         
-        public TBuilder Where_HandleCommandAsync_fails<TCommand>(Error error)
+        public TBuilder Where_HandleCommandAsync_fails<TCommand>(Error error) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
                 .ReturnsAsync(Response.Failure<TDto>(error));
             return (TBuilder) this;
+        }        
+        public Error Where_HandleCommandAsync_fails<TCommand>() where TCommand : notnull
+        {
+            var error = Error.Create(Guid.NewGuid().ToString());
+            Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
+                .ReturnsAsync(Response.Failure<TDto>(error));
+            return error;
         }
         
-        public TBuilder Where_HandleCommandAsync_throws<TCommand>(Exception exception)
+        public TBuilder Where_HandleCommandAsync_throws<TCommand>(Exception exception) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleCommandAsync(It.IsAny<TCommand>()))
                 .ThrowsAsync(exception);
             return (TBuilder) this;
         }
-        public void VerifyHandleCommandAsync<TCommand>(Expression<Func<TCommand, bool>> predicate, int times = 1)
+        public void VerifyHandleCommandAsync<TCommand>(Expression<Func<TCommand, bool>> predicate, int times = 1) where TCommand : notnull
         {
             Mock.Verify(x => x.HandleCommandAsync(It.Is(predicate)), Times.Exactly(times));
         }
