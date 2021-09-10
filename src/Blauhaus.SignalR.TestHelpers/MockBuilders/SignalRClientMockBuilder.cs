d320d2ef39436d6e0c2df133374fee58ac2f3e34
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-09-10 12:48:48 +0200

    no message

--- src/Blauhaus.SignalR.TestHelpers/MockBuilders/SignalRClientMockBuilder.cs
+++ src/Blauhaus.SignalR.TestHelpers/MockBuilders/SignalRClientMockBuilder.cs
@@ -10,34 +10,52 @@ namespace Blauhaus.SignalR.TestHelpers.MockBuilders
 {
     public class SignalRClientMockBuilder : BaseAsyncPublisherMockBuilder<SignalRClientMockBuilder, ISignalRClient, SignalRConnectionState>
     {
-        public SignalRClientMockBuilder Where_HandleAsync_returns<TCommand>(Response response)
+        public SignalRClientMockBuilder Where_HandleAsync_returns<TCommand>(Response response) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleAsync(It.IsAny<TCommand>())).ReturnsAsync(response);
             return this;
         }
-        public SignalRClientMockBuilder Where_HandleAsync_fails<TCommand>(Error error)
+        public SignalRClientMockBuilder Where_HandleAsync_fails<TCommand>(Error error) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleAsync<TCommand>(It.IsAny<TCommand>())).ReturnsAsync(Response.Failure(error));
             return this;
         }
+        public Error Where_HandleAsync_fails<TCommand>() where TCommand : notnull
+        {
+            var error = Error.Create(Guid.NewGuid().ToString());
+            Mock.Setup(x => x.HandleAsync<TCommand>(It.IsAny<TCommand>())).ReturnsAsync(Response.Failure(error));
+            return error;
+        }
          
-        public void VerifyHandleAsync<TCommand>(Expression<Func<TCommand, bool>> predicate)
+        public void VerifyHandleAsync<TCommand>(Expression<Func<TCommand, bool>> predicate) where TCommand : notnull
         {
             Mock.Verify(x => x.HandleAsync(It.Is(predicate)));
         }
 
-        public SignalRClientMockBuilder Where_HandleAsync_succeeds<TCommand, TResponse>(TResponse response)
+        public SignalRClientMockBuilder Where_HandleAsync_succeeds<TCommand, TResponse>(TResponse response) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleAsync<TCommand, TResponse>(It.IsAny<TCommand>())).ReturnsAsync(Response.Success(response));
             return this;
         }
-        public SignalRClientMockBuilder Where_HandleAsync_fails<TCommand, TResponse>(Error error)
+        public SignalRClientMockBuilder Where_HandleAsync_succeeds<TCommand, TResponse>(Func<TResponse> response) where TCommand : notnull
+        {
+            Mock.Setup(x => x.HandleAsync<TCommand, TResponse>(It.IsAny<TCommand>()))
+                .ReturnsAsync(()=> Response.Success(response.Invoke()));
+            return this;
+        }
+        public SignalRClientMockBuilder Where_HandleAsync_fails<TCommand, TResponse>(Error error) where TCommand : notnull
         {
             Mock.Setup(x => x.HandleAsync<TCommand, TResponse>(It.IsAny<TCommand>())).ReturnsAsync(Response.Failure<TResponse>(error));
             return this;
         }
+        public Error Where_HandleAsync_fails<TCommand, TResponse>() where TCommand : notnull
+        {
+            var error = Error.Create(Guid.NewGuid().ToString());
+            Mock.Setup(x => x.HandleAsync<TCommand, TResponse>(It.IsAny<TCommand>())).ReturnsAsync(Response.Failure<TResponse>(error));
+            return error;
+        }
          
-        public void VerifyHandleAsync<TCommand, TResponse>(Expression<Func<TCommand, bool>> predicate)
+        public void VerifyHandleAsync<TCommand, TResponse>(Expression<Func<TCommand, bool>> predicate) where TCommand : notnull
         {
             Mock.Verify(x => x.HandleAsync<TCommand, TResponse>(It.Is(predicate)));
         }
